8484ade8e9d0c699fa1e609f2e25a2e8d591cd99
Viggo Kristensen <viggo.kristensen@hotmail.com>
2024-11-20 15:36:34 +0100

    Added user list and ability to log in and also ability for admin to create user.

---
+++ MandalorianBankomaten/Admin.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata;
+
+namespace MandalorianBankomaten
+{
+    public class Admin
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public Admin(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+
+        public List<User> CreateUser(List<User> users)
+        {
+            Console.Write("Vänligen skriv in ett användarnamn");
+            string newusername = Console.ReadLine().ToLower();
+
+            bool usernameExists = users.Any(user => user.Name.ToLower() == newusername);
+
+            if (usernameExists)
+            {
+                Console.WriteLine("Användarnamnet är redan upptaget. Försök med ett annat.");
+                return users;
+            }
+            else
+            {
+                Console.Write("Vänligen skriv in ett lösenord");
+                string newpswd = Console.ReadLine();
+                User newUser = new User(newusername, newpswd);
+                users.Add(newUser);
+                Console.WriteLine("Användaren har skapats!");
+                return users;
+            }
+        }
+    }
+}
---
+++ MandalorianBankomaten/Bank.cs
@@ -0,0 +1,80 @@
+﻿namespace MandalorianBankomaten
+{
+    public class Bank
+    {
+        public List<User> users = new List<User>
+        {
+            new User("viggo", "1234"),
+            new User("egzon", "1234"),
+            new User("alex", "1234"),
+            new User("margarita", "1234"),
+            new User("tim", "1234"),
+            new User("arbunit", "1234"),
+        };
+
+        public void Run()
+        {
+            bool loginSuccesfull = LogIn();
+        }
+
+        public bool LogIn()
+        {
+            int attempts = 0;
+            while (attempts < 3)
+            {
+                Console.Write("Vänligen skriv in ditt användernamn: ");
+                string username = Console.ReadLine().ToLower();
+                Console.Write("Vänligen skriv in ditt lösenord: ");
+                string userpswd = ReadPassword();
+
+                foreach (var user in users)
+                {
+                    if(username == user.Name && userpswd == user.Password)
+                    {
+                        Console.WriteLine("Inloggning lyckades!");
+                        return true;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Inloggning misslyckades, försök igen!");
+            }
+            return false;
+        }
+
+        static string ReadPassword()
+        {
+            string password = string.Empty;
+
+            while (true)
+            {
+                // Read a single character
+                ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+                // Check if the key is Enter
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                // Check if the key is Backspace
+                else if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (password.Length > 0)
+                    {
+                        // Remove the last character from the password
+                        password = password[..^1];
+                        // Move the cursor back, overwrite the '*' and move back again
+                        Console.Write("\b \b");
+                    }
+                }
+                else
+                {
+                    // Add the character to the password
+                    password += keyInfo.KeyChar;
+                    // Display an asterisk
+                    Console.Write('*');
+                }
+            }
+            return password;
+        }
+    }
+}
