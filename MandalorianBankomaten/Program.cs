3c482a489e755721272ae52e94605e3555fa2a9c
alexanderek <Alexander.ek@live.se>
2024-11-20 13:30:02 +0100

    Skapat upp user och account för att kunna lägga in test användare och loopa igenom lista för deras personliga konton som visar kontonamn och saldo.

--- MandalorianBankomaten/Program.cs
+++ MandalorianBankomaten/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var user = new User("Mandalorian", "1234");
+            
+            user.Accounts.Add(new Account("Lönekonto", 10000, "SEK"));
+            user.Accounts.Add(new Account("Sparkonto", 50000, "SEK"));
+
+            // Visa användarens konton och saldon
+            user.ShowAccounts();
+
+            // Skapa en annan användare utan konton
+            User user2 = new User("Yoda", "1111");
+
+            // Visa hans konton
+            user2.ShowAccounts();
+            
         }
     }
 }
