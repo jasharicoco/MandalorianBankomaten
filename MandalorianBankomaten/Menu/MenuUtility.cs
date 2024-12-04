ec2b59e74125ab360767ff5e6d07ba4b916d542e
Ozonlagret <tim.drejhede@gmail.com>
2024-12-04 19:02:22 +0100

    Added a method that creates a text box that adapts to the given message length

--- MandalorianBankomaten/Menu/MenuUtility.cs
+++ MandalorianBankomaten/Menu/MenuUtility.cs
@@ -7,43 +7,47 @@
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("\n====================================================");
-            Console.WriteLine("❌ Du har gjort för många misslyckade försök. Kontot är tillfälligt avstängt.");
-            Console.WriteLine("====================================================");
+            TextBox("❌ Du har gjort för många misslyckade försök. Kontot är tillfälligt avstängt.");
             Console.ResetColor();
         }
         public static void ShowFailedLoginMessage(int remainingAttempts)
         {
-            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n====================================================");
-            Console.WriteLine("❌ Inloggning misslyckades!");
-            Console.WriteLine("====================================================");
-            Console.ResetColor();
-            Console.WriteLine($"Försök kvar: {remainingAttempts}");
+            TextBox("❌ Inloggning misslyckades!");
+            Console.WriteLine($"\nFörsök kvar: {remainingAttempts}");
             System.Threading.Thread.Sleep(1500); // Pause program for user interaction
         }
         public static void ShowSuccessMessage(string userName)
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\n====================================================");
-            Console.WriteLine($"✅ Inloggning lyckades! Välkommen {userName}!");
-            Console.WriteLine("====================================================");
-            Console.ResetColor();
+            TextBox($"✅ Inloggning lyckades! Välkommen {userName}!");
             System.Threading.Thread.Sleep(1500); // Pause program for user interaction
         }
         public static void ShowSuccessMessageAdmin(string userName)
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\n====================================================");
-            Console.WriteLine($"✅ Inloggning lyckades! Välkommen admin: {userName}!");
-            Console.WriteLine("====================================================");
-            Console.ResetColor();
+            TextBox($"✅ Inloggning lyckades! Välkommen admin: {userName}!");
             System.Threading.Thread.Sleep(1500); // Pause program for user interaction
         }
-
+        public static void TextBox(string message)
+        {
+            Console.Clear();
+            bool loopCount = true;
+            for (int i = 0; i < message.Length + 7; i++)
+            {
+                Console.Write("=");
+                if (i == message.Length + 6 && loopCount)
+                {
+                    Console.WriteLine($"\n|  {message}  |");
+                    loopCount = false;
+                    i = 0;
+                }
+            }
+            Console.WriteLine("=");
+            Console.ResetColor();
+        }
         // (int, ConsoleKey) means the method can return both of those types. 
         public static (int, ConsoleKey) ShowMenu(string[] menu, int index, ConsoleKey key) 
         {
